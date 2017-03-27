ee7f9caef9650988201d501e3bc389a5c1492131
tim5046 <tim5046@gmail.com>
2017-03-27 13:16:30 -0700

    complete version and help

--- calc.js
+++ calc.js
@@ -1,15 +1,33 @@
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
+var Calculator = require('./lib/Calculator');
+var calculator = new Calculator;
+
 var commands = []
 
 process.stdin.on('data', function(str) {
   str = str.trim();
-  if (str === 'q' || str === 'quit') {
-    console.log('Goodbye');
-    process.exit();
-  } else {
-    callFunction(str)
+  handleCalcInfo(str);
+  // push the commands and arguments to `commands` and then do something with them
+
+  // handle -v and -h by checking `str` before pushing to `commands` and run a *different* function to output the results
+
+
+  // if (str === 'q' || str === 'quit') {
+  //   console.log('Goodbye');
+  //   process.exit();
+  // } else {
+  //   callFunction(str)
+  // }
+})
+
+function handleCalcInfo(str){
+  var version = /\-v|\-\-version/;
+  var help = /\-h|\-\-help/;
+  if (str.match(version)){
+    console.log("Version: " + calculator.version)
+  } else if (str.match(help)){
+    console.log("Help: " + calculator.help)
   }
 }
-
