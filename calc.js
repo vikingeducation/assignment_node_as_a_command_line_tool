14a18450b6ffd624e7092c8fdd6bf5e734e26dc8
William Whitworth <william10charles@gmail.com>
2017-03-27 12:36:46 -0600

    some pseudo code has been completed

---
+++ calc.js
@@ -0,0 +1,12 @@
+//maping will be done with stdin resume()
+// use argv[2] operation
+//read through the array and
+// if its !number it an operation
+//    use switch to verify it is one of the defined operations
+//    u
+// if (-v || --version)
+//print version 1.0.0
+//else if( -h || --help)
+//print enter lorem ipsom
+//else if(add)
+//
