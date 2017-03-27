4cf49213a15009c8dd3ec0922f2d88e7ebcbf269
tim5046 <tim5046@gmail.com>
2017-03-27 13:32:10 -0700

    wip

--- calc.js
+++ calc.js
@@ -4,14 +4,29 @@ process.stdin.setEncoding('utf8');
 var Calculator = require('./lib/Calculator');
 var calculator = new Calculator;
 
-var commands = []
+
 
 process.stdin.on('data', function(str) {
   str = str.trim();
-  handleCalcInfo(str);
-  // push the commands and arguments to `commands` and then do something with them
+  if (calculatorHandlers.handleCalcInfo(str)) {
+    process.exit();
+  } else {
+    var commands = []
+  // handle str if it's not -v or -h
+  // str looks something like "add 5 5"
+    commands.push({
+      str.split(' ')[0]: [str.split(' ')[1], str.split(' ')[2]]
+    })
 
-  // handle -v and -h by checking `str` before pushing to `commands` and run a *different* function to output the results
+
+  }
+
+  // push the commands and arguments to `commands` and then do something with them
+   commands = [
+   {'add': [1,2]},
+   {'add': [3,4]}
+   ]
+
 
 
   // if (str === 'q' || str === 'quit') {
@@ -22,12 +37,37 @@ process.stdin.on('data', function(str) {
   // }
 })
 
-function handleCalcInfo(str){
-  var version = /\-v|\-\-version/;
-  var help = /\-h|\-\-help/;
-  if (str.match(version)){
-    console.log("Version: " + calculator.version)
-  } else if (str.match(help)){
-    console.log("Help: " + calculator.help)
+var calculatorHandlers = {
+  // refactor the returns here
+  handleCalcInfo: function(str){
+    var version = /\-v|\-\-version/;
+    var help = /\-h|\-\-help/;
+    if (str.match(version)){
+      console.log("Version: " + calculator.version);
+      return true
+    } else if (str.match(help)){
+      console.log("Help: " + calculator.help)
+      return true
+    } else {
+      return false;
+    }
+  },
+
+  add: function(obj){
+    return obj.add[0] + obj.add[1];
+  },
+
+  subtract: function(obj){
+    return obj.sub[0] - obj.sub[1];
+  },
+
+  div: function(obj){
+    return obj.div[0] / obj.div[1];
+  },
+
+  mult: function(obj){
+    return obj.mult[0] * obj.add[1];
   }
 }
+
+
