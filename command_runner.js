bfa6b416e4bb2d1b5d141082548e3ca26824aac2
Egle Libby <eglelibby@gmail.com>
2017-03-27 15:51:05 -0400

    command runner update

--- command_runner.js
+++ command_runner.js
@@ -3,28 +3,20 @@ process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
 var child_process = require('child_process');
-var user_input = [];
 
 process.stdin.on('data', function(str) {
   str = str.trim();
 
-  if (str === '\\q') {
-  
-    user_input.forEach((input) => {
-      console.log(input);
-      // var cmd = child_process.spawn(input[0], input.splice(0));
-      
-      // cmd.stdout.on('data', (data) => {
-
-      //   console.log(data);
-      // });
-    })
+  if (str === 'q' || str === 'quit') {
     console.log('Goodbye');
     process.exit();
     
   } else {
-    user_input.push(str.split(' '));
-    console.log(user_input);
+    str = str.split(' ');
+    var cmdSpawn = child_process.spawn(str[0], str.slice(1));
+    cmdSpawn.stdout.on('data', (data) => {
+      console.log(`${ data }`);
+    });
   }
 
 });
