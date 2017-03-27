0a24e202f84539deca4a3d0d350962d8b5354433
Nicholas Romeo <romeonicholas@gmail.com>
2017-03-27 11:01:45 -0700

    Finish io exercise

--- command_runner.js
+++ command_runner.js
@@ -1,8 +1,5 @@
-const fs = require('fs');
-
 console.log("Started process");
 process.stdin.resume();
-
 process.stdin.setEncoding('utf8');
 
 
@@ -10,5 +7,21 @@ process.stdin.setEncoding('utf8');
 process.stdin.on('data', function(str) {
   str = str.trim();
 
+  if (str === 'q' || str === 'quit') {
+    process.exit();
+  }
+  else {
+    var input = str.split(" ");
+    var cmd = cp.spawn(input[0], input.slice(1,input.length));
 
+    cmd.stdout.on('data', function(data) {
+      console.log(`${data}`);
+    })
+
+    cmd.stderr.on('data', function(data) {
+      console.error(`Error: ${data}`);
+    })
+  }
 });
+
+var cp = require('child_process');
