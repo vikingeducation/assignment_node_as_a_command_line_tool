7be7123165f13218ccac5bbca00e22cc8748003a
Nicholas Romeo <romeonicholas@gmail.com>
2017-03-27 13:59:18 -0700

    Add logs to find breakage

--- calc/calc.js
+++ calc/calc.js
@@ -2,7 +2,7 @@
 //  with stdin listener
 
 //How will you properly pass arguments to subcommands to the appropriate function?
-//  
+//
 
 //How will you enable subcommand chaining?
 
@@ -17,32 +17,43 @@ process.stdin.setEncoding('utf8');
 var cp = require('child_process');
 var pkgjson = require('./package.json')
 
+var inputFormat = function(input) {
+  var input = str.split(" ");
+  // var command = input[0];
+  // var args = input.slice(1);
+  console.log(input);
+  return input;
+}
+
+
+
 process.stdin.on('data', function(str) {
   str = str.trim();
 
+  // call object keys if there are a lot of if/elses
   if (str === 'q' || str === 'quit') {
     process.exit();
   } else if (str === '-v' || str === '--version') {
-    console.log(pkgjson.version) 
+    console.log(pkgjson.version)
   } else if (str === '-h' || str === '--help') {
     console.log(pkgjson.help)
   }
 
-
-
   else {
+    // var formattedInput = inputFormat(str);
+    // console.log();
     inputFormat(str);
+    //console.log(input);
+
+    //Perform calculator functions
   }
 });
 
+// var add = function(a, b) {
+//   return a + b;
+// }
 
-var inputFormat = function(input) {
-  var input = str.split(" ");
-  var command = input[0];
-  var args = input.slice(1);
-}
 
-    
 
 
 
