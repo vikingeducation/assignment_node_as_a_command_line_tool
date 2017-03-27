5400cc3f86c161e268bc6bcbe854cdb0a791cfa0
Jerry Gao <gao@blackwright.com>
2017-03-27 13:29:36 -0400

    finish child process warmup

--- basic_child_processes.js
+++ basic_child_processes.js
@@ -1,3 +1,33 @@
-const cp = require('child-process');
+const cp = require('child_process');
 
-var cmd = cp.spawn('ps -aux');
+// var cmd = cp.spawn('ps', ['aux']);
+
+// cmd.stdout.on('data', (data) => {
+//   console.log(`Data: ${ data }`);
+// });
+
+// cmd.stderr.on('data', (err) => {
+//   console.error(`Error: ${ err }`);
+// });
+
+// cmd.on('close', (code) => {
+//   console.log(`Closed with code: ${ code }`);
+// });
+
+var cmd = cp.exec('ps aux', (err, stdout, stderr) => {
+  if (err) {
+    console.error(err.stack);
+    console.error(`Error code: ${ err.code }`);
+    console.error(`Signal received: ${ err.signal }`);
+  }
+
+  if (stderr) {
+    console.error(`Child Process STDERR: ${ stderr }`);
+  }
+
+  console.log(`Child process STDOUT: ${stdout}`);
+});
+
+cmd.on('exit', (code) => {
+  console.log(`Child process exited with code: ${ code }`);
+});
