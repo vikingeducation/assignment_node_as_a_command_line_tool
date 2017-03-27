04731ed38e2c5aa273a10cad505f99223dc4799e
markmarkyesyes <markhahn545@gmail.com>
2017-03-27 12:33:57 -0500

    tomato

---
+++ command_runner.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+console.log("Started process");
+process.stdin.resume();
+
+process.stdin.setEncoding('utf8');
+
+
+
+process.stdin.on('data', function(str) {
+  str = str.trim();
+
+
+});
