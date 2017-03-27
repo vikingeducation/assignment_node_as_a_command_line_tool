033153c272232af677eddbd74d0057566032ac84
William Whitworth <william10charles@gmail.com>
2017-03-27 16:53:47 -0600

    finishded calc & command_runner

--- calc.js
+++ calc.js
@@ -10,82 +10,60 @@
 //print enter lorem ipsom
 //else if(add)
 //
-//const calcInfoArr = ["-v", "--version", "-h", "--help"];
+
 const calcOps = ["add", "sub", "mult", "div", "pow"];
 const specialOps = ["sqrt", "abs", "ln", "log", "sin", "cos", "tan"];
 var inputArr = process.argv;
 var debugFlag = false;
 var result = 0;
 var start = 0;
-// if (inputArr[2] === "-v" || inputArr[2] === "--version") {
-//   console.log("Calculator version is: 1.0.0");
-// } else if (inputArr[2] === "-h" || inputArr[2] === "--help") {
-//   console.log(
-//     "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-//   );
-// } else
+
 initialCheck(inputArr[2]);
+
 function runCalc() {
   if (!calcOps.includes(inputArr[2]) && !specialOps.includes(inputArr[2])) {
     console.log("Invalid input");
-  }
-  else {
-    if(specialOps.includes(inputArr[2])){
+  } else {
+    if (specialOps.includes(inputArr[2])) {
       result = doSpecialCalc(inputArr[2], inputArr[3]);
       start = 4;
       if (debugFlag) {
         console.log(`${inputArr[2]} ${inputArr[3]} => ${result}`);
       }
-    }
-    else {
+    } else {
       result = doCalc(inputArr[2], inputArr[3], inputArr[4]);
       start = 5;
       if (debugFlag) {
-        console.log(`${inputArr[2]} ${inputArr[3]} ${inputArr[4]} => ${result}`);
+        console.log(
+          `${inputArr[2]} ${inputArr[3]} ${inputArr[4]} => ${result}`
+        );
       }
     }
-      // if (debugFlag) {
-      //   console.log(`${inputArr[2]} ${inputArr[3]} ${inputArr[4]} => ${result}`);
-      // }
+
     for (var i = start; i < inputArr.length; i += 2) {
-      if ((!calcOps.includes(inputArr[i]) && !specialOps.includes(inputArr[i])) || isNaN(inputArr[i + 1])) {
+      var tempResult = result;
+      if (specialOps.includes(inputArr[i])) {
+        result = doSpecialCalc(inputArr[i], result);
+
+        if (debugFlag) {
+          console.log(`${inputArr[i]} ${tempResult} => ${result}`);
+        }
+        i--;
+      } else if (calcOps.includes(inputArr[i])) {
+        result = doCalc(inputArr[i], result, inputArr[i + 1]);
+        if (debugFlag) {
+          console.log(
+            `${inputArr[i]} ${tempResult} ${inputArr[i + 1]} => ${result}`
+          );
+        }
+      } else {
         result = "Invalid Input";
         break;
-      }
-      var tempResult = result;
-      result = doCalc(inputArr[i], result, inputArr[i + 1]);
-      if (debugFlag) {
-        console.log(
-          `${inputArr[i]} ${tempResult} ${inputArr[i + 1]} => ${result}`
-        );
       }
     }
     console.log("Calculator output is: " + result);
   }
 }
-
-// if (!calcOps.includes(inputArr[2])) {
-//   console.log("Invalid input");
-// } else {
-//   result = doCalc(inputArr[2], inputArr[3], inputArr[4]);
-//   if (debugFlag) {
-//     console.log(`${inputArr[2]} ${inputArr[3]} ${inputArr[4]} => ${result}`);
-//   }
-//   for (var i = 5; i < inputArr.length; i += 2) {
-//     if (!calcOps.includes(inputArr[i]) || isNaN(inputArr[i + 1])) {
-//       result = "Invalid Input";
-//       break;
-//     }
-//     var tempResult = result;
-//     result = doCalc(inputArr[i], result, inputArr[i + 1]);
-//     if (debugFlag) {
-//       console.log(
-//         `${inputArr[i]} ${tempResult} ${inputArr[i + 1]} => ${result}`
-//       );
-//     }
-//   }
-//   console.log("Calculator output is: " + result);
-// }
 
 function initialCheck(input) {
   switch (input) {
@@ -102,15 +80,15 @@ function initialCheck(input) {
     case "-d":
     case "--debug":
       debugFlag = true;
-      inputArr.splice(2,1); // removing "-d" or "--debug" from the inputArr
+      inputArr.splice(2, 1); // removing "-d" or "--debug" from the inputArr
     default:
       runCalc();
   }
 }
 
 function doCalc(op, val1, val2) {
-  val1 = parseInt(val1);
-  val2 = parseInt(val2);
+  val1 = Number(val1);
+  val2 = Number(val2);
   switch (op) {
     case "add":
       return val1 + val2;
@@ -126,7 +104,7 @@ function doCalc(op, val1, val2) {
 }
 
 function doSpecialCalc(op, val) {
-  val = parseInt(val);
+  val = Number(val);
   switch (op) {
     case "sqrt":
       return Math.sqrt(val);
--- command_runner.js
+++ command_runner.js
@@ -1,23 +1,34 @@
-const cp = require('child_process');
+const cp = require("child_process");
 
 process.stdin.resume();
 
-process.stdin.setEncoding('utf8');
+process.stdin.setEncoding("utf8");
 
-process.stdin.on('data', (str) => {
+process.stdin.on("data", str => {
   str = str.trim();
 
   if (str == "q" || str === "quit") {
     console.log("goodbye");
     process.exit();
-  }
-  else {
-    var cmd = cp.spawn('echo', [str]);
-    cmd.stdout.on ('data', (data) => {
-      console.log(`Data: ${ data }`);
+  } else {
+    var parsedInput = parseInput(str);
+    var cmd = cp.spawn(parsedInput.command, parsedInput.args);
+    cmd.stdout.on("data", data => {
+      console.log(`Data: ${data}`);
     });
-    cmd.stderr.on('data', (data) => {
-      console.error(`Error: ${ data }`);
+    cmd.stderr.on("data", data => {
+      console.error(`Error: ${data}`);
     });
   }
 });
+
+function parseInput(inputStr) {
+  var array = inputStr.split(" ");
+  var command = array[0];
+  var args = array.slice(1);
+
+  return {
+    command,
+    args
+  };
+}
