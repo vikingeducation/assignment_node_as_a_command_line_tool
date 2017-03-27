7e3509fbf75c6ef8c215044d13d8617250debb9c
William Whitworth <william10charles@gmail.com>
2017-03-27 14:33:05 -0600

    calc progress

--- calc.js
+++ calc.js
@@ -12,13 +12,41 @@
 //
 const calcInfoArr = ["-v", "--version", "-h", "--help"];
 const calcOps = ["add", "sub", "mult", "div"];
+var inputArr = process.argv;
+var result = 0;
 
-for(var i=0; i<process.argv.length; i++){
-  if (process.argv[i] === "-v" || process.argv[i] === "--version" ){
+for (var i = 0; i < inputArr.length; i++) {
+  if (inputArr[i] === "-v" || inputArr[i] === "--version") {
     console.log("Calculator version is: 1.0.0");
-  }
-  else if (process.argv[i] === "-h"|| process.argv[i] === "--help"){
-    console.log("Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+  } else if (inputArr[i] === "-h" || inputArr[i] === "--help") {
+    console.log(
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    );
+  } else if (
+    calcOps.includes(inputArr[i]) && calcOps.includes(inputArr[i + 2])
+  ) {
+    result = doCalc(inputArr[i], result, inputArr[i + 1]);
+    i += 1;
+    console.log(result);
+    //doCalc = function(arguenst)
+  } else {
+    result = doCalc(inputArr[i], inputArr[i + 1], inputArr[i + 2]);
+    i += 2;
+    console.log(result);
   }
   //console.log(`${index}: ${val}`);
+}
+function doCalc(op, val1, val2) {
+  val1 = parseInt(val1);
+  val2 = parseInt(val2);
+  switch (op) {
+    case "add":
+      return val1 + val2;
+    case "sub":
+      return val1 - val2;
+    case "mult":
+      return val1 * val2;
+    case "div":
+      return val1 / val2;
+  }
 }
