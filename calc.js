698a20fae18ca8eefee97a42e223397d01b62c25
Renzo Tomlinson <rttomlinson@gmail.com>
2017-04-09 00:24:56 +0000

    Add flags and additional calc functions

--- calc.js
+++ calc.js
@@ -1,89 +1,81 @@
 "use strict";
-
+const versionNumber = '1.0.0';
+const cp = require("child_process");
 //get input from process.argsv
 let cliArgs = Array.from(process.argv).slice(2);
-//if the first cli argument begins with a - or --
-//regex to match any value beginning with - or --
-// /^-{1,2}/
-
-let regexFlags = new RegExp(/^-{1,2}/);
-
-//map it to the appropriate function to output to the user
-//if the first value matches our regexp
-if (regexFlags.test(cliArgs[0])) {
-    //extract the flag
-    let flag;
-    //Check the it matches exactly two dashes
-    if (/^-{2}/.test(cliArgs[0])) {
-        flag = cliArgs[0].substring(2);
-    } else {
-        flag = cliArgs[0].substring(1);
-    }
-    
-    
-    //if so, substring starting at 2
-    //else, substring starting at 1
-    switch (flag) {
-        case "v":
-        case "version":
-            //send to version subprocess
-            console.log("Here is the version!");
-            break;
-        case "h":
-        case "help":
-            //send to help text
-            console.log("Here is the help docs!");
-            break;
-        default:
-            console.log("That is not a valid flag");
-    }
+let flags = {
+    "version": false,
+    "help": false,
+    "debug": false,
+    "interactive": false,
+};
+//set the flags found on the script call
+flagsSetter(cliArgs);
+if (flags.version) {
+    console.log(`CALC.JS v${versionNumber}`);
+    process.exit();
 }
-
+if (flags.help) {
+    //grab help manual
+    let manual = require("./help/manual.json");
+    let command = cliArgs[0];
+    if (command) {
+        console.log(`${ command }: ${ manual[command] }`);
+    }
+    else {
+        console.log(JSON.stringify(manual, null, 2));
+    }
+    process.exit();
+}
+if (flags.interactive) {
+    let cmd = cp.spawn('node', ['./calc_runner.js']);
+}
 //create regex to only match calculator functions
-let calcFunctionsRegex = new RegExp(/add|sub|mult|div/);
+let calcFunctionsRegex = new RegExp(/add|sub|mult|div|pow|sqrt/);
 if (calcFunctionsRegex.test(cliArgs[0])) {
-    //turn cliArgs into an array of grouped functions and values
-    let groups = [];
-    //Grab the first group
-    groups.push(cliArgs.splice(0,3));
-
-    while(cliArgs.length > 0) {
-        groups.push(cliArgs.splice(0,3));
-
+    //turn arguments in sets
+    let setsOfCommands = [];
+    while (cliArgs.length > 0) {
+        let calcSet = [];
+        if (cliArgs[0].match(/sqrt/i) && setsOfCommands.length > 0) {
+            calcSet.push(cliArgs[0]);
+            cliArgs.splice(0, 1);
+        }
+        else if (setsOfCommands.length == 0 && !cliArgs[0].match(/sqrt/i)) {
+            calcSet.push(cliArgs[0], cliArgs[1], cliArgs[2]);
+            cliArgs.splice(0, 3);
+        }
+        else {
+            calcSet.push(cliArgs[0], cliArgs[1]);
+            cliArgs.splice(0, 2);
+        }
+        setsOfCommands.push(calcSet);
     }
-    let total = groups.reduce(function (acc, element) {
-        
-        let value = calculate(element);
-        return value += acc;
+    //Grab the first set of commands for the calculator
+    let total = setsOfCommands.reduce((acc, args) => {
+        //if args length is two, we need to add the initial value at position 1
+        if ((args.length == 2 && !(args[0] == "sqrt")) || args.length == 1) {
+            args.splice(1, 0, acc);
+        }
+        let newValue = calculate(args);
+        if (flags.debug) {
+            if (args[0] == "sqrt") {
+                console.log(`${ args[0] } ${ args[1] } => ${ newValue }`);
+            }
+            else {
+                console.log(`${ args[0] } ${ args[1] } ${ args[2] } => ${ newValue }`);
+            }
+        }
+        return newValue;
     }, 0);
-    
-
-    console.log("The total was", total);
-    return total;
+    console.log(total);
 }
-
-
-
-
-console.log(cliArgs);
-
-
-
-
-
-
-
-
 //aliasing will be done by referencing the same function
-
-
-
-function calculate(cliArgs) {
-    let calcFunc = cliArgs[0];
+function calculate(args) {
+    let calcFunc = args[0];
     let value;
-    let a = Number(cliArgs[1]);
-    let b = Number(cliArgs[2]);
-    
+    let a = Number(args[1]);
+    let b = Number(args[2]);
     switch (calcFunc) {
         case "add":
             value = a + b;
@@ -97,9 +89,60 @@ function calculate(cliArgs) {
         case "mult":
             value = a * b;
             break;
+        case "pow":
+            value = Math.pow(a, b);
+            break;
+        case "sqrt":
+            value = Math.sqrt(a);
+            break;
         default:
             console.log("That is not a valid calc function");
-        
     }
     return value;
+}
+
+function flagsSetter(args) {
+    //check for all the flags and set the flags object as appropriate
+    let regexFlags = new RegExp(/^-{1,2}/);
+    //map it to the appropriate function to output to the user
+    //if the first value matches our regexp
+    if (regexFlags.test(args[0])) {
+        //extract the flag
+        let flag;
+        //Check the it matches exactly two dashes
+        if (/^-{2}/.test(args[0])) {
+            flag = args[0].substring(2);
+        }
+        else {
+            flag = args[0].substring(1);
+        }
+        //if so, substring starting at 2
+        //else, substring starting at 1
+        switch (flag) {
+            case "v":
+            case "version":
+                //send to version subprocess
+                flags.version = true;
+                break;
+            case "h":
+            case "help":
+                //send to help text
+                flags.help = true;
+                break;
+            case "d":
+            case "debug":
+                console.log("Debug mode!");
+                flags.debug = true;
+                break;
+            case "i":
+            case "interactive":
+                console.log("Interactive mode!");
+                flags.interactive = true;
+                break;
+            default:
+                console.log("That is not a valid flag");
+        }
+        //remove the arguments from the args list
+        args.splice(0, 1);
+    }
 }
--- calc_runner.js
+++ calc_runner.js
@@ -1,44 +1,37 @@
 "use strict";
-const fs = require('fs');
 const cp = require("child_process");
 //Start listening for user input from the command line.
 process.stdin.resume();
 //Set character encoding of incoming input
 process.stdin.setEncoding('utf8');
-let userInput = '';
+let runningTotal = 0;
 //Listen to the data event. Occurs when the user hits return
 process.stdin.on('data', function(str) {
     //Clean any leading or trailing whitespace
     str = str.trim();
-    //parse string into individual values. divide on spaces
     //Check if the user wants to quit
-    if (str == '\\q' || str == 'quit') {
+    if (str == 'done') {
         console.log("Exiting program. Good bye");
-        //Write user input to file
-        try {
-            fs.mkdirSync('./data');
-        }
-        catch (err) {
-            //Should check type of err message and if it is because directory already created. Then continue.
-            console.log("Assume error message related to directory already being created. Safe to continue.");
-        }
-        //Check if data folder exists in the root director
-        //If not, make it
-        //Save the user input to the input.txt file in the data/ directory
-        fs.appendFileSync('./data/input.txt', userInput);
         process.exit();
     }
     else {
+        //parse string into individual values. divide on spaces
+        str = str.split(" ");
+        str.unshift('./calc.js');
+        if (str.length < 4) {
+            str.splice(2, 0, runningTotal);
+        }
         //Print out the user's input
         //addUserInput(str);
         //spawn new child process
         //new child process will output user input
-        const cmd = cp.spawn('echo', [str]);
+        const cmd = cp.spawn('node', str);
         cmd.on('error', (err) => {
             console.error(`${ err.stack }`);
         });
         cmd.stdout.on('data', (data) => {
-            console.log(`Data: ${ data }`);
+            runningTotal = parseInt(data);
+            console.log(`${ runningTotal }`);
         });
         cmd.stderr.on('data', (data) => {
             console.error(`STDERR: ${ data }`);
@@ -46,7 +39,5 @@ process.stdin.on('data', function(str) {
         cmd.on('close', (code) => {
             console.log(`Child process spawn existed with code: ${ code }`);
         });
-        console.log(str + " added to input!");
-        userInput += str + "\n";
     }
 });
