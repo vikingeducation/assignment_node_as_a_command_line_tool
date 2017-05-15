4e05ba0432653304a52a7e9b54e0ec0243da9262
Tyler Ketron <tketron@gmail.com>
2017-05-14 20:46:52 -0700

    add, sub, div, mult arithemetic functionality

--- calc.js
+++ calc.js
@@ -3,11 +3,19 @@ var ch = require('./calc_helper');
 var args = process.argv;
 if (args.length > 2) {
   args.forEach( (arg, index, args) => {
-    console.log(arg);
+    // console.log(arg);
     if (arg === '-v' || arg === '--version') {
       console.log(ch.VERSION_NUMBER);
     } else if (arg === '-h' || arg === '--help') {
       console.log (ch.HELP_TEXT);
+    } else if (arg === 'add') {
+      console.log(ch.add( args[index+1], args[index+2]));
+    } else if (arg === 'sub') {
+      console.log(ch.sub(args[index+1], args[index+2]));
+    } else if (arg === 'div') {
+      console.log(ch.div(args[index+1], args[index+2]));
+    } else if (arg === 'mult') {
+      console.log(ch.mult(args[index+1], args[index+2]));
     }
   });
 }
--- calc_helper.js
+++ calc_helper.js
@@ -1,8 +1,19 @@
 var calc_helper = {
+  add: function(number1, number2) {
+    return parseInt(number1) + parseInt(number2);
+  },
 
+  sub: function(number1, number2) {
+    return parseInt(number1) - parseInt(number2);
+  },
 
+  div: function(number1, number2) {
+    return parseInt(number1) / parseInt(number2);
+  },
 
-
+  mult: function(number1, number2) {
+    return parseInt(number1) * parseInt(number2);
+  },
 
   VERSION_NUMBER:'1.0.0',
   HELP_TEXT: 'Input a '
