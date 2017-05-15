21da202b5d9e6a53a5500d79069f0ca4d030757d
Tyler Ketron <tketron@gmail.com>
2017-05-15 09:30:31 -0700

    refactor more control flow into helper class

--- calc.js
+++ calc.js
@@ -2,20 +2,24 @@ var ch = require('./calc_helper');
 
 var args = process.argv;
 if (args.length > 2) {
+  let tally = 0;
   args.forEach( (arg, index, args) => {
     // console.log(arg);
-    if (arg === '-v' || arg === '--version') {
-      console.log(ch.VERSION_NUMBER);
-    } else if (arg === '-h' || arg === '--help') {
-      console.log (ch.HELP_TEXT);
-    } else if (arg === 'add') {
-      console.log(ch.add( args[index+1], args[index+2]));
-    } else if (arg === 'sub') {
-      console.log(ch.sub(args[index+1], args[index+2]));
-    } else if (arg === 'div') {
-      console.log(ch.div(args[index+1], args[index+2]));
-    } else if (arg === 'mult') {
-      console.log(ch.mult(args[index+1], args[index+2]));
+    if (arg.startsWith('-')) {
+      console.log(ch.getStaticText(arg));
+    } else {
+      var result = ch.performCalculation(arg, args[index+1], args[index+2]);
+      tally += result;
     }
+    // } else if (arg === 'add') {
+    //   console.log(ch.add( args[index+1], args[index+2]));
+    // } else if (arg === 'sub') {
+    //   console.log(ch.sub(args[index+1], args[index+2]));
+    // } else if (arg === 'div') {
+    //   console.log(ch.div(args[index+1], args[index+2]));
+    // } else if (arg === 'mult') {
+    //   console.log(ch.mult(args[index+1], args[index+2]));
+    // }
   });
+  console.log(tally);
 }
--- calc_helper.js
+++ calc_helper.js
@@ -1,22 +1,43 @@
 var calc_helper = {
+  performCalculation: function(operation, number1, number2) {
+    if (operation === 'add') {
+      return this.add(parseInt(number1), parseInt(number2));
+    } else if (operation === 'sub') {
+      return this.subtract(parseInt(number1), parseInt(number2));
+    } else if (operation === 'div') {
+      return this.divide(parseInt(number1), parseInt(number2));
+    } else if (operation === 'mult') {
+      return this.multiply(parseInt(number1), parseInt(number2));
+    } else return 0;
+  },
+
   add: function(number1, number2) {
-    return parseInt(number1) + parseInt(number2);
+    return (number1 + number2);
   },
 
-  sub: function(number1, number2) {
-    return parseInt(number1) - parseInt(number2);
+  subtract: function(number1, number2) {
+    return (number1 - number2);
   },
 
-  div: function(number1, number2) {
-    return parseInt(number1) / parseInt(number2);
+  divide: function(number1, number2) {
+    return (number1 / number2);
   },
 
-  mult: function(number1, number2) {
-    return parseInt(number1) * parseInt(number2);
+  multiply: function(number1, number2) {
+    return (number1 * number2);
+  },
+
+  getStaticText: function(arg) {
+    if (arg === '-v' || arg === '--version') {
+      return this.VERSION_NUMBER;
+    } else if (arg === '-h' || arg === '--help') {
+      return this.HELP_TEXT;
+    } else return this.INVALID_ARGUMENT;
   },
 
   VERSION_NUMBER:'1.0.0',
-  HELP_TEXT: 'Input a '
+  HELP_TEXT: 'Input a ',
+  INVALID_ARGUMENT: 'Invalid argument'
 }
 
 module.exports = calc_helper;
