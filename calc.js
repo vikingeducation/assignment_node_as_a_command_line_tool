e884e45b8a640045e0f49e8eb4130a0013700a24
Tyler Ketron <tketron@gmail.com>
2017-05-15 22:46:39 -0700

    implemented functionality to chain arithmetic methods together

--- calc.js
+++ calc.js
@@ -3,23 +3,17 @@ var ch = require('./calc_helper');
 var args = process.argv;
 if (args.length > 2) {
   let tally = 0;
-  args.forEach( (arg, index, args) => {
-    // console.log(arg);
+  for (var index=2; index<args.length; index++) {
+    var arg = args[index];
     if (arg.startsWith('-')) {
-      console.log(ch.getStaticText(arg));
-    } else {
-      var result = ch.performCalculation(arg, args[index+1], args[index+2]);
-      tally += result;
+    } else if (!arg.match(/\d/)) {
+      if (tally > 0) {
+        var result = ch.performCalculation(arg, tally, args[index+1]);
+        tally = result;
+      } else {
+        var result = ch.performCalculation(arg, args[index+1], args[index+2]);
+        tally += result;
+      }
     }
-    // } else if (arg === 'add') {
-    //   console.log(ch.add( args[index+1], args[index+2]));
-    // } else if (arg === 'sub') {
-    //   console.log(ch.sub(args[index+1], args[index+2]));
-    // } else if (arg === 'div') {
-    //   console.log(ch.div(args[index+1], args[index+2]));
-    // } else if (arg === 'mult') {
-    //   console.log(ch.mult(args[index+1], args[index+2]));
-    // }
-  });
-  console.log(tally);
+  }
 }
