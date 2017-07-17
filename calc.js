3b796f444c9fbaa857d69a1775b08fd36668b5e6
Ian Halverson <halversoncode@gmail.com>
2017-07-17 16:59:13 -0400

    basic calc functions done

--- calc.js
+++ calc.js
@@ -1,4 +1,57 @@
-// use process.argv
 
+// use process.argv
+//loop
 // check for keywords like add/sub/div/mult
 // depending keyword, carry out operations for all the numbers following until we hit a string
+
+
+function calc(){
+    var sum = 0;
+    var func = "";
+    var arg = process.argv;
+    for(let i=2;i<arg.length;i++) {
+    if (!(isNaN(arg[i]))) {
+      var num = parseInt(arg[i]);
+      switch(func) {
+        case "add":
+          sum = (sum + num);
+        break;
+        case "sub":
+          sum = (sum - num);
+        break;
+        case "mult":
+          sum = (sum * num);
+        break;
+        case "div":
+          sum = (sum / num);
+        break;
+      }
+    }
+    else if (typeof arg[i] === "string") {
+      switch(arg[i]) {
+        case "add":
+          func = "add";
+        break;
+        case "sub":
+          func = "sub";
+        break;
+        case "div":
+          if (i === 2) {
+            sum = arg[3];
+            i++;
+          }
+          func = "div";
+        break;
+        case "mult":
+          if (i === 2) {
+            sum = 1;
+          }
+          func = "mult";
+        break;
+      }
+    }
+  }
+  console.log(sum);
+}
+
+calc();
