5fc741915b1dda39bb2968e64a9d2a365712d18c
Alex-Willenbrink <awillenbrink@gmail.com>
2017-07-17 17:41:51 -0500

    finished refactoring

--- assignment_4/calc.js
+++ assignment_4/calc.js
@@ -1,9 +1,9 @@
 let input = process.argv;
 
-
 let oper = process.argv[2]; // first argument
-let isDebug = false, startId = 3;
+let isDebug = false, startId = 3; // startId will be for num1
 
+// help options if needed
 if(oper === "-h" || oper === "--help") {
   console.log("This is a help message");
   process.exit();
@@ -14,56 +14,62 @@ if(oper === "-v" || oper === "--version") {
   process.exit();
 }
 
+// special debug case (shift num1 and operator)
 if (oper === '-d' || oper === '--debug') {
   isDebug = true;
   oper = process.argv[3];
   startId ++;
 }
 
-let num1 = process.argv[startId];
-let result = num1;
-for(let i = startId + 1; i < process.argv.length; i += 2) {
-  let num2;
+let elements = process.argv.slice(startId + 1); // elements is [num2 ..] or [sqrt ...]
+console.log(calc (process.argv[startId], oper, elements));
 
-  if (oper === 'sqrt') {
-    num2 = 0;
-    i --;
-  }
-  else num2 = process.argv[i];
-  
-  result = specialMath(num1, num2, oper);
+function calc (num1, op, args) {
+  let result, num2;
 
-  if (isDebug) {
-    print(oper, num1, num2, result);
-  }
-  num1 = result;
-
-  if(process.argv.length > i + 1) {
-    oper = process.argv[i + 1];
-  }
-}
-if (oper == 'sqrt') {
-  result = specialMath(num1, 0, oper);
-  if (isDebug) {
-    print(oper, num1, 0, result);
-  }
-}
-
-console.log(result);
-
-
-function print (op, num1, num2, result) {
-  if (op === 'sqrt') {
-    console.log(op + ' ' + num1 + ' => ' + result);  
+  // is operator sqrt?
+  if (isSingleArgument(op)) {
+    result = specialMathForSingle(num1, op);
   }
   else {
-    console.log(op + ' ' + num1 + ' ' + num2 + ' => ' + result);    
+    num2 = args[0];
+    args = args.slice(1);
+    result = specialMath(num1, num2, op);
   }
+
+  if (isDebug) {
+    print (op, num1, num2, result);
+  }
+
+  // do we have any arguments left?
+  if (args.length > 0)
+    return calc(result, args[0], args.slice(1));
+
+  return result;
+}
+
+function isSingleArgument(op) {
+  return (op === 'sqrt');
+}
+
+function print (op, num1, num2, result) {
+  if (isSingleArgument(op)) {
+    console.log(op + ' ' + num1 + ' => ' + result);
+  }
+  else {
+    console.log(op + ' ' + num1 + ' ' + num2 + ' => ' + result);
+  }
+}
+
+function specialMathForSingle(num1, op) {
+  return specialMath(num1, 0, op);
 }
 
 function specialMath(num1, num2, op) {
   num1 = Number(num1);
   num2 = Number(num2);
+
+  // print(op, num1, num2, 0);
 
   if(op === "add") {
     return num1 + num2;
@@ -79,7 +85,7 @@ function specialMath(num1, num2, op) {
       throw new Error("This is another error");
     }
     return num1 / num2;
-  } 
+  }
   if (op === 'pow') {
     return Math.pow(num1, num2);
   }
@@ -87,5 +93,5 @@ function specialMath(num1, num2, op) {
     return Math.sqrt(num1);
   }
 
-  throw new Error("This is an error"); 
+  throw new Error("This is an error");
 }
