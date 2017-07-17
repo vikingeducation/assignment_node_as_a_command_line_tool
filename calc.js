909fcb4e6c74d497fdfc4b185a029bb494fb0b9d
Benjamin Soung <soungbenjamin@gmail.com>
2017-07-17 17:14:42 -0400

    node cmd calc finished

--- calc.js
+++ calc.js
@@ -28,30 +28,64 @@ function calc(){
       }
     }
     else if (typeof arg[i] === "string") {
-      switch(arg[i]) {
-        case "add":
-          func = "add";
-        break;
-        case "sub":
-          func = "sub";
-        break;
-        case "div":
-          if (i === 2) {
-            sum = arg[3];
-            i++;
-          }
-          func = "div";
-        break;
-        case "mult":
-          if (i === 2) {
-            sum = 1;
-          }
-          func = "mult";
-        break;
-      }
+    	if (arg[i] === '-h' || arg[i] === '--help') {
+    		return 'To use the calculate, type: node calc.js <operator> <numbers>. These operations are chainable.';
+    	} 
+
+    	if (arg[i] === '-v' || arg[i] === '--version') {
+    		return '0.0.1';
+
+    	} else {
+
+	      switch(arg[i]) {
+	        case "add":
+	          func = "add";
+	        break;
+	        case "sub":
+	          func = "sub";
+	        break;
+	        case "div":
+	          if (i === 2) {
+	            sum = arg[3];
+	            i++;
+	          }
+	          func = "div";
+	        break;
+	        case "mult":
+	          if (i === 2) {
+	            sum = 1;
+	          }
+	          func = "mult";
+	        break;
+	      }
+    	}
     }
   }
-  console.log(sum);
+
+  return sum;
 }
 
-calc();
+function calcOutput() {
+	console.log(calc());
+}
+
+calcOutput();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
