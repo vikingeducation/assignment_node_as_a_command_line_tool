bd6b1e7e3fdceb913a41198df35960318e787471
Benjamin Soung <soungbenjamin@gmail.com>
2017-07-17 16:08:07 -0400

    Finished command runner exercise

--- command_runner.js
+++ command_runner.js
@@ -5,17 +5,33 @@ process.stdin.resume();
 process.stdin.setEncoding("utf8");
 
 var input;
-var args = [];
 
 process.stdin.on('data', (data) => {
   data = data.trim();
+
+  if (data === 'q' || data === 'quit') {
+    console.log('Bye!');
+    process.exit();
+  }
+
   input = data;
   input = input.split(' ');
-  for (i=1;i<input.length;i++) {
-    args.push(input[i]);
-  }
-  let cmd = cp.spawn(input[0], args);
-  cmd.stdout.on(data, (data) => {
-    console.log(`Data: ${ data }`);
+
+  logIt(input);
+});
+
+function logIt(input) {
+  let cmd = cp.spawn(input[0], input.slice(1));
+
+  cmd.stdout.on('data', (data) => {
+    console.log(`${ data }`);
+  });
+
+  cmd.stderr.on('data', (data) => {
+    console.error(`${ data }`);
+  });
+
+  cmd.on('error', (err) => {
+    throw err;
   })
-})
+}
