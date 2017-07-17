ebca70a6ea68538f3585bff3a85fc189707c59b2
Benjamin Soung <soungbenjamin@gmail.com>
2017-07-17 18:13:39 -0400

    optional in progress

--- calc.js
+++ calc.js
@@ -1,64 +1,113 @@
-
 // use process.argv
 //loop
 // check for keywords like add/sub/div/mult
 // depending keyword, carry out operations for all the numbers following until we hit a string
 
 
-function calc(){
-    var sum = 0;
-    var func = "";
-    var arg = process.argv;
-    for(let i=2;i<arg.length;i++) {
+function calc() {
+  let sum = 0;
+  let func = "";
+  let arg = process.argv;
+  let debugMode = 0;
+  let debugOutput = "";
+
+  if (arg[2] === '--debug' || arg[2] === '-d') {
+    debugMode = 1;
+  }
+
+  for (let i = 2; i < arg.length; i++) {
     if (!(isNaN(arg[i]))) {
-      var num = parseInt(arg[i]);
-      switch(func) {
+      let num = parseInt(arg[i]);
+      switch (func) {
         case "add":
+        	debugOutput += ` ${arg[i]}`;
+        	// console.log(debugOutput)
           sum = (sum + num);
-        break;
+          break;
         case "sub":
+        	debugOutput += ` ${arg[i]}`;
           sum = (sum - num);
-        break;
+          break;
         case "mult":
+        	debugOutput += ` ${arg[i]}`;
           sum = (sum * num);
-        break;
+          break;
         case "div":
+        	debugOutput += ` ${arg[i]}`;
           sum = (sum / num);
-        break;
+          break;
       }
-    }
-    else if (typeof arg[i] === "string") {
-    	if (arg[i] === '-h' || arg[i] === '--help') {
-    		return 'To use the calculate, type: node calc.js <operator> <numbers>. These operations are chainable.';
-    	} 
+    } else if (typeof arg[i] === "string") {
+      if (arg[i] === '-h' || arg[i] === '--help') {
+        return 'To use the calculate, type: node calc.js <operator> <numbers>. These operations are chainable.';
+      }
 
-    	if (arg[i] === '-v' || arg[i] === '--version') {
-    		return '0.0.1';
+      if (arg[i] === '-v' || arg[i] === '--version') {
+        return '0.0.1';
 
-    	} else {
+      } else {
 
-	      switch(arg[i]) {
-	        case "add":
-	          func = "add";
-	        break;
-	        case "sub":
-	          func = "sub";
-	        break;
-	        case "div":
-	          if (i === 2) {
-	            sum = arg[3];
-	            i++;
-	          }
-	          func = "div";
-	        break;
-	        case "mult":
-	          if (i === 2) {
-	            sum = 1;
-	          }
-	          func = "mult";
-	        break;
-	      }
-    	}
+        switch (arg[i]) {
+          case "add":
+            func = "add";
+
+            if (debugMode === 1) {
+            	debugOutput += ` = ${sum}`;
+              console.log(debugOutput);
+
+              debugOutput = func;
+            } 
+
+            break;
+          case "sub":
+
+            func = "sub";
+
+            if (debugMode === 1) {
+            	debugOutput += ` = ${sum}`;
+              console.log(debugOutput);
+
+              debugOutput = func;
+            } 
+
+            break;
+          case "div":
+
+
+
+            if (i === 2) {
+              sum = arg[3];
+              i++;
+            }
+
+            func = "div";
+
+            if (debugMode === 1) {
+            	debugOutput += ` = ${sum}`;
+              console.log(debugOutput);
+
+              debugOutput = func;
+            } 
+
+            break;
+          case "mult":
+
+            if (i === 2) {
+              sum = 1;
+            }
+
+            func = "mult";
+
+            if (debugMode === 1) {
+            	debugOutput += ` = ${sum}`;
+              console.log(debugOutput);
+
+              debugOutput = func;
+            } 
+
+            break;
+        }
+      }
     }
   }
 
@@ -66,26 +115,7 @@ function calc(){
 }
 
 function calcOutput() {
-	console.log(calc());
+  console.log(calc());
 }
 
 calcOutput();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
