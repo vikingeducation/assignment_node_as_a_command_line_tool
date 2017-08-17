af3be2ad48d19549ab6ad97ca451e97e7e919b5a
Brandon <bparkerproductions@gmail.com>
2017-08-17 18:00:06 -0500

    can now calculate result, included with unlimited chaining

--- calc.js
+++ calc.js
@@ -1,23 +1,97 @@
-/*
-	Take stdinput, when i > 2, parse next args and stop after first or second number.
-	If there are more args, parse them the same way and push into args array
-	args array would look something like [add 2 2, sub 5, mul 6 5]
+var help_string = `
+	Welcome to my calculator!
+	Get a result with:
+	[operator] [num] [num]
 
-	Pass args array into calculate function.
+	operators include add, sub, mult, div
 
-	**in calculate function**
-	map each operater with add, sub, etc.
-	perform the calulation and return the results, using something like eval()
+	You can also chain methods, just add more operators straight
+	after your first calculable result.
+	Feel free to chain as many methods as you like!
 
-	for chained methods, add each results and alter a results value. When all arguments 
-	are finished, return results
-	
-	example:
-	[add 5 5, sub 2]
-	results = 10;
-	results = 8
-	return results
+	Chained methods work when there is one operator followed by a number.
 
-	for v and version commands, use an if statement that checks first, then processes
-	results in an else block
-*/
+	Example:
+	add 4 4 mult 6 sub 7 //41
+
+	**More Examples**
+	//a simple operation
+	node calc.js sub 7 8 //-1
+
+	//Chaining multiple arguments
+	node calc.js add 1 2 sub 7 mult 10 //-40
+
+	Have fun!
+`
+
+var pjson = require('./package.json');
+
+var version_arg = process.argv[2].trim();
+var result = 0;
+
+if(version_arg == "-v" || version_arg == "-version"){
+	console.log(pjson.version);
+}
+else if(version_arg == "-h" || version_arg == "-help"){
+	console.log(help_string)
+}
+
+else{
+	parse_args();
+}
+
+function parse_args(){
+	var results = "";
+	var first_split = true;
+	var p = process.argv;
+
+	//delete first two file path args
+	process.argv.splice(0,2);
+
+	while(process.argv.length > 1){
+
+		if(first_split){
+			results = `${p[0]} ${p[1]} ${p[2]}`;
+			first_split = false;
+			process.argv.splice(0,3);
+		}
+		else{
+			results = `${p[0]} ${p[1]}`
+			process.argv.splice(0,2);
+		}
+
+		calc_results(results);
+		results = "";
+	}
+}
+
+function calc_results(val){
+	val = val.split(" ");
+	var first_num = parseInt(val[1]);
+	var second_num = parseInt(val[2]);
+
+	if(val[0] == "add"){
+		!second_num ? result+=first_num : result+=(first_num + second_num);
+	}
+	else if(val[0] == "sub"){
+		!second_num ? result-=first_num : result+=(first_num - second_num);
+	}
+	else if(val[0] == "mult"){
+		!second_num ? result*=first_num : result+=(first_num * second_num);
+	}
+	else if(val[0] == "div"){
+		!second_num ? result/=first_num : result+=(first_num / second_num);
+	}
+	log_output(first_num, second_num, val[0]);
+}
+
+	function log_output(first_num, second_num, operator){
+		var ops = {"add": "+", "sub":"-", "mult": "*", "div": "/"}
+
+		if(second_num){
+			console.log(`${first_num} ${ops[operator]} ${second_num} = ${result}`);
+		}
+		else{
+			console.log( `${ops[operator]} ${first_num} = ${result}`);
+		}
+	}
