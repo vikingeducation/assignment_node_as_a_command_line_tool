959a112a9af2499f0b63dca642f909fb78eee493
Chuck Michael <chuckqs@yahoo.com>
2017-08-17 13:49:00 -0400

    Calculator with chaining commands finished

--- calc.js
+++ calc.js
@@ -26,6 +26,58 @@ print solution
 if (process.argv[2] === '-h' || process.argv[2] === '--help') {
   console.log("---calc.js help---\nEnter node calc.js (then add,sub, mult, div) then (a)(a) \nExample: node calc.js add 5 5\n      or node calc.js add 5 5 add 6\n         Then the result will be printed\n---END---");
 }
-if (process.argv[2] === '-v' || process.argv[2] === '--version') {
+else if (process.argv[2] === '-v' || process.argv[2] === '--version') {
   console.log(process.version);
+}
+else {
+  var solution = 0;
+  argumentList = process.argv;
+  argumentList.shift();
+  argumentList.shift();
+
+  while (argumentList[0] != undefined){
+    //if 2 arguments
+    if ( (isNaN(argumentList[1]) === false) && (isNaN(argumentList[2]) === false)) {
+      if (argumentList[0] === 'add') {
+        solution = Number(argumentList[1]) + Number(argumentList[2]) + solution;
+      }
+      else if (argumentList[0] === 'sub') {
+        solution = Number(argumentList[1]) - Number(argumentList[2]) + solution;
+      }
+      else if (argumentList[0] === 'mult') {
+        solution = Number(argumentList[1]) * Number(argumentList[2]) + solution;
+      }
+      else if (argumentList[0] === 'div') {
+        solution = Number(argumentList[1]) / Number(argumentList[2]) + solution;
+      }
+      else {
+        console.log("Error, please see --help");
+      }
+      argumentList.shift();
+      argumentList.shift();
+      argumentList.shift();
+    }
+    //if 1 argument
+    else if (isNaN(argumentList[1]) === false) {
+      if (argumentList[0] === 'add') {
+        solution += Number(argumentList[1]);
+      }
+      else if (argumentList[0] === 'sub') {
+        solution -= Number(argumentList[1]);
+      }
+      else if (argumentList[0] === 'mult') {
+        solution *= Number(argumentList[1]);
+      }
+      else if (argumentList[0] === 'div') {
+        solution /= Number(argumentList[1]);
+      }
+      else {
+        console.log("Error, please see --help");
+      }
+      argumentList.shift();
+      argumentList.shift();
+    }
+
+  }
+  console.log(solution);
 }
