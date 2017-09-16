05345d8468706af5ea748d162c7fd2b6b09dd7e1
Thomas Hauge <thomas.sommerfelt.hauge@gmail.com>
2017-09-16 14:12:27 +0200

    pow and sqrt

--- calc.js
+++ calc.js
@@ -1,39 +1,83 @@
-let args = process.argv.slice(2, process.argv.length);
-
 const operators = {
   add: '+',
   sub: '-',
   mult: '*',
-  div: '/'
+  div: '/',
+  pow: 'pow',
+  sqrt: 'sqrt'
 };
-
 var version = '1.0.0';
 var help = `Command-Line Calculator
 =======================
-Use commands and arguments to perform calculations
-and receive the result in the command-line.
+Perform calculations and receive the result on the
+command-line. You may chain commands together.
 
 The following commands are available:
   Addition:       add (a) (b)
   Subtraction:    sub (a) (b)
   Multiplication: mult (a) (b)
-  Division:       div (a) (b)`;
+  Division:       div (a) (b)
+  Exponents:      pow (a) (b)
+  Square root:    sqrt (a)`;
+
+let debugMode = false;
+let args = process.argv.slice(2, process.argv.length);
+let i = 0;
 
 if (args[0] === '-v' || args[0] === '--version') {
   console.log(version);
 } else if (args[0] === '-h' || args[0] === '--help') {
   console.log(help);
 } else {
-  let operator = operators[args[0]];
-  let expression = args[1] + operator + args[2];
-  let result = eval(expression);
-  let i = 3;
+  if (args[0] === '-d' || args[0] === '--debug') {
+    debugMode = true;
+    i += 1;
+  };
+  let a = args[i + 1];
+  let b = args[i + 2];
+  let func = [args[i]];
+  let operator = operators[func];
+  var result;
+  var expression;
+
+  if (operator === 'pow') {
+    result = Math.pow(a,b);
+    i += 3;
+  } else if (operator === 'sqrt') {
+    result = Math.sqrt(a);
+    b = '';
+    i += 2;
+  } else {
+    expression = a + operator + b;
+    result = eval(expression);
+    i += 3;
+  };
+
+  if (debugMode) {
+    console.log(`${func} ${a} ${b} => ${result}`);
+  };
 
   while (i < args.length) {
-    operator = operators[args[i]];
-    expression = result + operator + args[i+1];
-    result = eval(expression);
-    i += 2;
-  }
+    a = result;
+    b = args[i + 1];
+    func = args[i];
+    operator = operators[func];
+
+    if (operator === 'pow') {
+      result = Math.pow(a,b);
+    } else if (operator === 'sqrt') {
+      result = Math.sqrt(a);
+      b = '';
+      i += 1;
+    } else {
+      expression = a + operator + b;
+      result = eval(expression);
+      i += 2;
+    };
+
+    if (debugMode) {
+      console.log(`${func} ${a} ${b} => ${result}`);
+    };
+  };
   console.log(result);
 };
