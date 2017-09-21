7074fbfe966113f6e0197478e6a22bf7db12d623
promilo <milind.shah@mail.utoronto.ca>
2017-09-21 15:02:08 -0400

    finished command raunner

---
+++ command_runner.js
@@ -0,0 +1,34 @@
+var fs = require('fs')
+
+process.stdin.resume()
+process.stdin.setEncoding('utf8')
+
+process.stdin.on('data', (str) => {
+  str = str.trim();
+  if (str === '\\q') {
+      console.log("quiting process")
+      process.exit();
+    } else {
+      // spawn code here.
+      var arrayArgs = str.split(' ')
+      var cp = require('child_process');
+
+      var cmd = cp.spawn(arrayArgs[0], arrayArgs.slice(1));
+
+      cmd.on('error', err => {
+        console.error(`${ err.stack }`);
+      });
+
+      cmd.stdout.on('data', data => {
+        console.log(`Data: ${ data }`);
+      });
+
+      cmd.stderr.on('data', data => {
+        console.error(`STDERR: ${ data }`);
+      });
+
+      cmd.on('close', code => {
+        console.log(`Child process exited with code: ${ code }`);
+      });
+    }
+  })
