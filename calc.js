bf8cc093c68e3595faaad93034ea272d5be641fe
William Huynh <31399549+wdh7@users.noreply.github.com>
2017-09-24 13:25:14 -0700

    Add calculator command chaining

--- calc.js
+++ calc.js
@@ -1,28 +1,37 @@
 const args = process.argv;
-const operation = args[2];
-const a = parseInt(args[3]);
-const b = parseInt(args[4]);
+const calcArgs = args.slice(2);
+let operation = calcArgs[0];
+let leftNum = calcArgs[1];
+let rightNum = calcArgs[2];
 
 
-let total = 0;
+function calc(leftNum, operation, rightNum) {
+  switch (operation) {
+    case 'add':
+      return eval(`${leftNum} + ${rightNum}`);
+    case 'sub':
+      return eval(`${leftNum} - ${rightNum}`);
+    case 'div':
+      return eval(`${leftNum} / ${rightNum}`);
+    case 'mult':
+      return eval(`${leftNum} * ${rightNum}`);
+  }
+}
 
-switch (operation) {
-  case 'add':
-    total = a + b;
-    process.stdout.write(total.toString() + '\n');
-    break;
-  case 'sub':
-    total = a - b;
-    process.stdout.write(total.toString() + '\n');
-    break;
-  case 'div':
-    total = a / b;
-    process.stdout.write(total.toString() + '\n');
-    break;
-  case 'mult':
-    total = a * b;
-    process.stdout.write(total.toString() + '\n');
-    break;
-  default:
-    process.stdout.write('Please try again!' + '\n');
+let returnedVal = calc(leftNum, operation, rightNum);
+
+
+if (calcArgs.length === 3) {
+  process.stdout.write(returnedVal.toString() + '\n');
+} else {
+
+  for (let i = 3; i <= calcArgs.length - 2; i += 2) {
+    operation = calcArgs[i];
+    rightNum = calcArgs[i + 1];
+    leftNum = returnedVal;
+
+    returnedVal = calc(leftNum, operation, rightNum);
+  }
+
+  process.stdout.write(returnedVal.toString() + '\n');
 }
