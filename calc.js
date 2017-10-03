cdd6ae37851db8291569e8b5ecc3408ab62041a7
DC <heyyodc-git@yahoo.com>
2017-10-03 14:47:01 -0700

    pseudo code on calculator calc.js

---
+++ calc.js
@@ -0,0 +1,12 @@
+
+	//listen in on stdin for user input
+	//if quit condition, exit right away
+	//otherwise keep going
+		//further parse through user input by getting rid of all whitespace
+		//case input = -v or --version, print version to stdout
+		//case input = -h or --help, print help to stdout
+		//case input in (add, sub, mult,div) 
+			//then perform calc
+			//print out answer
+	//this whole thing loops due to listener on stdin for data event
+	
