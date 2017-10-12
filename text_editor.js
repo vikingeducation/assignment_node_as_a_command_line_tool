0d25659fd7097ea73209af0a5f37f354c0dcc2f1
Jared Gebel <jaredjgebel@gmail.com>
2017-10-12 16:01:19 -0400

    Complete STDIN listener

---
+++ text_editor.js
@@ -0,0 +1,17 @@
+var fs = require("fs");
+var cp = require("child_process");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+
+process.stdin.on("data", function(input) {
+  input = input.trim();
+  if (input === "\\q") {
+    console.log("Your text has been saved.");
+    process.exit();
+  } else {
+    fs.appendFile("./data/input.txt", input, err => {
+      if (err) throw err;
+    });
+  }
+});
