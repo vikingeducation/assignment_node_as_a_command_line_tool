85437447cddd98419ca061b9c541d2012f45937b
Jared Gebel <jaredjgebel@gmail.com>
2017-10-17 19:01:46 -0400

    Enable command chaining

--- calc.js
+++ calc.js
@@ -1,59 +1,75 @@
 var child_process = require("child_process");
 
-// Version number and help
-if (process.argv[2] === "-v" || process.argv[2] === "--version") {
-  console.log("1.0");
-} else if (process.argv[2] === "-h" || process.argv[2] === "--help") {
-  console.log(`To use the calculator, enter an operator and two numbers
-to perform the operation on. Subcommands can be performed by adding an
-additional parameter and another number to be executed on the current total.`);
-}
-
-// Define
-/* While the current index is less than or equal to the total number of arguments
-If there's not a running totals (if it's not a number yet)
-    Take the current index (start at 2) and pass it in as operations.blank
-    Pass in next two argvs as a and b
-    Save result to running total variable
-    Add 3 to current index
-  If running total is a number
-    Take current index and pass it in as operations.blank
-    Pass in current total as a and next index as b
-    Return result to total variable
-  */
+// Initiate variables for calculator total and correct starting index
+var total;
+var index = 2;
 
 // Define operation functions
-var operations = {
+var ops = {
   add: function(a, b) {
     return Number(a) + Number(b);
   },
-  subtract: function(a, b) {
+  sub: function(a, b) {
     return a - b;
   },
-  multiply: function(a, b) {
+  mult: function(a, b) {
     return a * b;
   },
-  divide: function(a, b) {
+  div: function(a, b) {
     return a / b;
   }
 };
 
-var total;
+// Perform each operation -- made so different
+// parameters plugged in in different circumstances
+var performOperation = function(operation, a, b) {
+  if (operation === "add") {
+    total = ops.add(a, b);
+  } else if (operation === "sub") {
+    total = ops.sub(a, b);
+  } else if (operation === "mult") {
+    total = ops.mult(a, b);
+  } else if (operation === "div") {
+    total = ops.div(a, b);
+  }
+  return total;
+};
 
-if (process.argv[2] === "add") {
-  total = operations.add(process.argv[3], process.argv[4]);
-  process.stdout.write(`Process STDOUT: ${total}
-`);
-} else if (process.argv[2] === "sub") {
-  total = operations.subtract(process.argv[3], process.argv[4]);
-  process.stdout.write(`Process STDOUT: ${total}
-`);
-} else if (process.argv[2] === "mult") {
-  total = operations.multiply(process.argv[3], process.argv[4]);
-  process.stdout.write(`Process STDOUT: ${total}
-`);
-} else if (process.argv[2] === "div") {
-  total = operations.divide(process.argv[3], process.argv[4]);
-  process.stdout.write(`Process STDOUT: ${total}
-`);
+// Version number and help menu options
+if (process.argv[2] === "-v" || process.argv[2] === "--version") {
+  console.log("1.0");
+} else if (process.argv[2] === "-h" || process.argv[2] === "--help") {
+  console.log(`To use the calculator, enter an operator and two numbers
+    to perform the operation on. Subcommands can be performed by adding an
+    additional parameter and another number to be executed on the current total.`);
 }
+
+// Loop to run calculations
+// Index is assigned 2 above in hoisted variable declarations
+// On the first set of parameters, pass in two numbers as the operands
+// On the second set of parameters, pass in the total and a new number as the operands
+while (index <= process.argv.length) {
+  if (index === 2) {
+    total = performOperation(process.argv[index], process.argv[index + 1], process.argv[index + 2]);
+    index += 3;
+  } else {
+    total = performOperation(process.argv[index], total, process.argv[index + 1]);
+    index += 2;
+  }
+}
+
+// Print final total
+process.stdout.write(`Process STDOUT: ${total}
+`);
+
+/* While the current index is less than or equal to the total number of arguments
+If there's not a running totals (if it's not a number yet)
+Take the current index (start at 2) and pass it in as operations.blank
+Pass in next two argvs as a and b
+Save result to running total variable
+Add 3 to current index
+If running total is a number
+Take current index and pass it in as operations.blank
+Pass in current total as a and next index as b
+Return result to total variable
+*/
