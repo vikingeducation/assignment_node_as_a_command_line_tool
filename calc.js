e51bb335a32284cdc2634b0cfce21b8ae3b3060f
Visiona <darbis@gmail.com>
2017-10-26 20:51:09 +0100

    Finlized node as cli Basic Calculator Functions

--- calc.js
+++ calc.js
@@ -4,58 +4,6 @@
 // How will you enable subcommand chaining?
 // How will you allow options like -h and -v and their aliases --help and --version to output the correct information?
 
-// console.log(process.argv[2]);
-
-// we get user input by accessing process.argv
-
-// var cp = require('child_process');
-
-
-var args = process.argv.slice(2, process.argv.length)
-var calc = {
-  // add: function(a,b) {
-  //   return a + b;
-  // },
-  // sub: function(a,b) {
-  //   return a - b;
-  // },
-  // div: function(a,b) {
-  //   return a/b;
-  // },
-  // mult: function(a,b) {
-  //   return a*b;
-  // },
-  // total: 0,
-}
-args.forEach( function(val, idx) {
-  if (val == '-h' || val == '--help' ) {
-    console.log('Explanation of how to use the calculator');
-  } else if (val == '-v' || val == '--version' ) {
-    console.log('1.0');
-  }
-  // var tempOp = 0;
-  // var val1, val2, total;
-  // if Number.isNaN( parseInt(val) ) {
-  //   if (val1 || val2) {
-  //
-  //   } else {
-  //     tempOp = calc.val();
-  //   }
-  // } else {
-  //   if (val1 == false && val1 == true  && idx != args.length-1) ) {
-  //     val1 = val;
-  //   } else if (val2 == false && val1 == true && idx != args.length-1) {
-  //     val2 = val;
-  //   } else if (idx == args.length-1) {
-  //     tempOp(val1, val2);
-  //   }
-  // }
-
-} )
-
-console.log(args);
-
-
 // if the value is integer, we use it as arguments
 // if the value is sub, add ... we will access function and add two follwoing arguments
 // I might enable subcommand chaining by iterating through every value, and holding temp score which will be passed further to other functions
@@ -63,3 +11,49 @@ console.log(args);
 // sub(total?, val)
 // -h, --help, --version, -v will be another data in the object accessible
 // we need stdout to return the value
+
+
+
+var args = process.argv.slice(2, process.argv.length);
+
+var calc = function (operation, a, b) {
+  switch (operation) {
+    case 'sub':
+      return a - b;
+      break;
+    case 'add':
+      return a + b;
+      break;
+    case 'mult':
+      return a*b;
+      break;
+    case 'div':
+      return a/b;
+      break;
+  }
+}
+
+var val1, val2, operation;
+
+args.forEach( function(val, idx) {
+
+  if (val == '-h' || val == '--help' ) {
+    console.log('Explanation of how to use the calculator');
+  } else if (val == '-v' || val == '--version' ) {
+    console.log('1.0');
+  }
+
+  if ( isNaN( parseInt(val) ) ) {
+      operation = val;
+  } else {
+    if (!val1) {
+      val1 = parseInt(val) ;
+    } else {
+      val2 = parseInt(val) ;
+    }
+  }
+})
+
+console.log( calc(operation, val1, val2) );
+
+console.log(args);
