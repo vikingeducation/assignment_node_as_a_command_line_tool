a53128f43567106989424ed1541cf2434df320fd
Johann <baptistajohann@gmail.com>
2017-11-13 10:20:13 -0800

    writeFile working

---
+++ text_editor.js
@@ -0,0 +1,19 @@
+let fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+process.stdin.on("data", function(str) {
+  str = str.trim();
+  
+  if (str === 'q' || str === 'quit') {
+    console.log('Goodbye.');
+    process.exit();
+  } 
+  fs.writeFile("data/input.txt", str, (err) => {
+    if (err) return console.log("error");
+    console.log("test > data/input.txt");
+  });
+
+
+});
