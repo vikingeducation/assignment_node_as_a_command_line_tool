ef388548339c931b02b1e942c733518cb7bcb157
Hai Nguyen <memyselfandhai@gmail.com>
2017-11-13 13:26:45 -0600

    quit added

---
+++ command_runner.js
@@ -0,0 +1,27 @@
+var  cp  = require('child_process');
+
+process.stdin.resume();
+
+process.stdin.setEncoding('utf8');
+
+var cmd;
+
+process.stdin.on('data', (data) => {
+  data = data.trim();
+
+  input_vars = data.split(' ');
+
+  if (data === 'q' || data == 'quit') {
+    console.log('Goodbye.');
+    process.exit();
+  } else {
+    cmd = cp.spawn(input_vars[0], [ input_vars[1,input_vars.length-1] ] )
+
+    cmd.stdout.setEncoding('utf8');
+
+    cmd.stdout.on('data', (data) => {
+      console.log(data)
+    })
+  }
+
+})
