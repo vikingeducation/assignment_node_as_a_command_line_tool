e51ccb0b302f9d2b0214285fa4fa95d8f8648063
Jeff Dederick <Xpressivmotion@gmail.com>
2017-11-13 17:54:12 -0500

    added pseudocode

--- calc.js
+++ calc.js
@@ -5,13 +5,28 @@ console.log(parseInt(bob));
 
 
 
-/*Capture command line arguments in array through argv. We will iterate through the values to determine whether the value is a string or integer.
-Push string values into string array, push number values into number array.  unshift first value in string array and compare/match to four string/math function values. 
-Once it matches, unshift first two numbers in number array and return value of math function performed on the numbers. Create a flag to */
+/*Capture command line arguments in array through argv.
 
+Create a flag to test if function has run yet.
 
+We will iterate through the values to determine whether the value is a string or integer.
 
+Push string values into string array, push number values into number array.
 
+unshift first value in string array and compare/match to four string/math function values.
 
+Once it matches, unshift first two numbers in number array and return value of math function performed on the numbers.
 
+Set flag to show calc has been run.
 
+Set previous result as new value a.
+
+Unshift next string from string array and compare to math strings.
+
+Unshift next number from number array and return value of math function perfomed on the numbers.
+
+Repeat.
+
+If string === -v || --verson : return version string.
+
+if string === -h || --help : return directions.
