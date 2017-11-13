29a148653304df072c621f9f26e4bd94f52c94ad
Johann <baptistajohann@gmail.com>
2017-11-13 13:27:10 -0800

    random

--- text_editor.js
+++ text_editor.js
@@ -8,7 +8,7 @@ let data = "";
 process.stdin.on("data", function(str) {
   str = str.trim();
   
-  if (str === 'q' || str === 'quit') {
+  if (str === '\\q' || str === '\\quit') {
     console.log('Goodbye.');
 
     fs.writeFile("data/input.txt", data, (err) => {
