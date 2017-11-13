8143bad02b08625cc58687ec571aca24f1ed3480
Jeff Dederick <Xpressivmotion@gmail.com>
2017-11-13 16:35:16 -0500

    tomato switch

--- command-runner.js
+++ command-runner.js
@@ -1,1 +1,16 @@
+'use strict';
+const cp = require('child_process');
 
+let output = '';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', str => {
+  str = str.trim();
+  output += str.split(' ');
+  let arr = output.slice(1);
+  const cmd = cp.spawn(output[0], arr);
+});
+
+process.stdout.setEncoding('utf8');
+process.stdout.on('data', vals => console.log(vals));
