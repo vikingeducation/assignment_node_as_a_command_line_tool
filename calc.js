1f1a46991e4de4924447c39c07e171477b709c13
edwinyung <eyung7@gmail.com>
2017-11-13 18:15:06 -0500

    calc updated

--- calc.js
+++ calc.js
@@ -7,8 +7,26 @@ process.stdin.setEncoding('utf8');
 process.stdin.on("data", function (str) {
     str = str.trim().split(" ");
     
-    let cmd = cp.spawn(str[0], [str[1],str[2]]);
+    //let cmd = cp.spawn('calculator', str);
 
+    //cmd.stdout.setEncoding('utf8');
+
+    process.stdout.on('data', (data) => {
+        console.log(data);
+    });
+
+    if (str[0] === '\\q' || str[0] === '\\quit') {
+        console.log('Goodbye.');
+        process.exit();
+    };
+
+});
+
+
+let calculator = function (arr) {
+    let first_comp;
+    let total;
+    
     let add = (a, b) => {
         return a + b;
     }
@@ -21,16 +39,40 @@ process.stdin.on("data", function (str) {
     let mult = (a, b) => {
         return a * b;
     }
-
-    cmd.stdout.setEncoding('utf8');
-
-    cmd.stdout.on('data', (data) => {
-        console.log(data);
-    });
-
-    if (str[0] === '\\q' || str[0] === '\\quit') {
-        console.log('Goodbye.');
-        process.exit();
-    };
-
-});
+    
+    switch (arr[0]) {
+        case "add":
+            first_comp = add(arr[1], arr[2]);
+            break;
+        case "sub":
+            first_comp = sub(arr[1], arr[2]);
+            break;
+        case "div":
+            first_comp = div(arr[1], arr[2]);
+            break;
+        case "mult":
+            first_comp = mult(arr[1], arr[2]);
+            break;
+    }
+    
+    total = first_comp;
+    
+    for (var i = 3; i < arr.length; i += 2) {
+        switch (arr[i]) {
+            case "add":
+                total = add(total, arr[i + 1]);
+                break;
+            case "sub":
+                total = sub(total, arr[i + 1]);
+                break;
+            case "div":
+                total = div(total, arr[i + 1]);
+                break;
+            case "mult":
+                total = mult(total, arr[i + 1]);
+                break;
+        }
+    }
+    
+    return total;
+}
