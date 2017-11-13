00cfae4e6d8dad9204bb3044938ff00f33febe20
GeneTinderholm <tinderholmgene@gmail.com>
2017-11-13 16:15:10 -0600

    I managed to screw it up

--- calc.js
+++ calc.js
@@ -2,7 +2,8 @@
 let argArray = process.argv.slice(2, (process.argv.length));
 let result = 0;
 let dummy = 0;
-
+let debug = 0;
+let selfFunction = function(argArray){
 if (argArray.length != 0) {
 //check the first statement(only one where version/etc are valid), also takes different # of params
   switch (argArray[0]) {
@@ -11,25 +12,41 @@ if (argArray.length != 0) {
       argArray.shift();
       argArray.shift();
       argArray.shift();
+      if(debug){
+        console.log(argArray[0], argArray[1], argArray[2], + " => " +result);
+      }
       break;
     case 'sub':
       result += (parseInt(argArray[1]) - parseInt(argArray[2]));
       argArray.shift();
       argArray.shift();
       argArray.shift();
+      if(debug){
+        console.log(argArray[0], argArray[1], argArray[2]+ " => " + result);
+      }
       break;
     case 'div':
       result += (parseInt(argArray[1]) / parseInt(argArray[2]));
       argArray.shift();
       argArray.shift();
       argArray.shift();
+      if(debug){
+        console.log(argArray[0], argArray[1], argArray[2]+ " => " + result);
+      }
       break;
     case 'mult':
       result += (parseInt(argArray[1]) * parseInt(argArray[2]));
       argArray.shift();
       argArray.shift();
       argArray.shift();
+      if(debug){
+        console.log(argArray[0], argArray[1], argArray[2]+ " => " + result);
+      }
       break;
+      case '--debug':
+        debug = 1;
+        selfFunction(argArray.shift());
+        break;
     case'-v':
       console.log("1.0.0")
       dummy = 1;
@@ -51,6 +68,7 @@ if (argArray.length != 0) {
       dummy = 1;
   }
 }
+}
 //check next statements, applies them to results
 while (argArray.length != 0 && dummy === 0) {
   switch (argArray[0]) {
@@ -58,25 +76,38 @@ while (argArray.length != 0 && dummy === 0) {
       result += + parseInt(argArray[1]);
       argArray.shift();
       argArray.shift();
+      if(debug){
+        console.log(argArray[0], result, + " => " +argArray[1]);
+      }
       break;
     case 'sub':
       result -= parseInt(argArray[1]);
       argArray.shift();
       argArray.shift();
+      if(debug){
+        console.log(argArray[0], result, + " => " +argArray[1]);
+      }
       break;
     case 'div':
       result /= parseInt(argArray[1]);
       argArray.shift();
       argArray.shift();
+      if(debug){
+        console.log(argArray[0], result, + " => " +argArray[1]);
+      }
       break;
     case 'mult':
       result *= parseInt(argArray[1]);
       argArray.shift();
       argArray.shift();
+      if(debug){
+        console.log(argArray[0], result, + " => " +argArray[1]);
+      }
       break;
     default:
       console.log("Please enter a valid function (add, sub, mult, div).");
       dummy = 1;
   }
 }
+selfFunction(argArray);
 console.log(result);
