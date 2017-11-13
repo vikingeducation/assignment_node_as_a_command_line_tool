60171a49af3c902d6408f1f126c88b5f103310c8
Steven Li <stevenli@Stevens-MacBook-Pro.local>
2017-11-13 10:49:15 -0800

    text-editor work

--- text-editor.js
+++ text-editor.js
@@ -1,9 +1,34 @@
 var fs=require('fs');
 
+/*
+function writePromise(){
+  console.log('writePromise started');
+  var writeToFile = new Promise(function(resolve, reject){
+    fs.writeFile('./data/input.txt', userInput, function(err){
+      if (err){
+        throw err;
+        console.log("error, something went wrong");
+      }
+    })
+  });
+  return writeToFile
+}
+*/
 
-var writeToFile = new Promise(function(resolve, reject){
+function writeToFile(words){
+  return new Promise(function(resolve, reject){
+    fs.writeFile('./data/input.txt', words, function(err){
+      if (err){
+        throw err;
+        console.log("error, something went wrong");
+        reject()
+      }
 
-});
+      console.log("The file was saved!");
+      resolve()
+    })
+  })
+}
 
 
 process.stdin.resume();
@@ -13,14 +38,21 @@ var userInput="";
 process.stdin.on('data', function(string){
   string=string.trim();
   if (string ==='\\q'|| string ==='quit'){
+    writeToFile(userInput).then((result) => {
+      process.exit()
+    });
+    /*
     fs.writeFile('./data/input.txt', userInput, function(err){
       if (err){
         throw err;
         console.log("error, something went wrong");
       }
-    }).then(function(){
+
+      console.log("The file was saved!");
+
       process.exit();
-    });
+    })
+    */
   }
   else{
     userInput += string;
