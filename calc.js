c8806cdfe38f70b8dbdd14a8e89262e3a13e290c
Jeff Dederick <Xpressivmotion@gmail.com>
2017-11-13 18:34:50 -0500

    tomato timer

--- calc.js
+++ calc.js
@@ -1,10 +1,4 @@
-process.stdin.resume();
-process.stdin.setEncoding('utf8');
-let bob = process.argv[2];
-console.log(parseInt(bob));
-
-
-
+'use strict';
 /*Capture command line arguments in array through argv.
 
 Create a flag to test if function has run yet.
@@ -29,4 +23,62 @@ Repeat.
 
 If string === -v || --verson : return version string.
 
-if string === -h || --help : return directions.
+if string === -h || --help : return directions.*/
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+let bob = process.argv.slice(2);
+
+let counter = 0;
+
+let result = 0;
+
+let george = (str, a, b) => {
+  if (counter === 0) {
+    console.log(bob.unshift());
+    a = bob.unshift();
+    b = bob.unshift();
+  } else if (bob.length > 0 && counter > 0) {
+    str = bob.unshift();
+    b = bob.unshift();
+  }
+
+  switch (bob[2]) {
+    case 'add':
+      result = a + b;
+      counter += 1;
+      break;
+    case 'sub':
+      result = a - b;
+      counter += 1;
+      break;
+    case 'div':
+      result = a / b;
+      counter += 1;
+      break;
+    case 'mult':
+      result = a * b;
+      counter += 1;
+    default:
+      console.log('Invalid math input');
+  }
+  switch (bob[2]) {
+    case 'add':
+      result += b;
+      break;
+    case 'sub':
+      result -= b;
+      break;
+    case 'div':
+      result /= b;
+      break;
+    case 'mult':
+      result *= b;
+    default:
+      console.log('Invalid math input');
+  }
+  process.stdout.setEncoding('utf8');
+  process.stdout.on('data', result => console.log(result));
+};
+
+george();
