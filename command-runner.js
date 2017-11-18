b53a32b760623b21fb3dbda54c82a8e6a1e064e6
Jeff Dederick <Xpressivmotion@gmail.com>
2017-11-18 10:46:06 -0500

    refactored command-runner

--- command-runner.js
+++ command-runner.js
@@ -1,21 +1,38 @@
 'use strict';
 const cp = require('child_process');
 
-let output = '';
+const newCP = (proc, arg) => {
+  let cmd = cp.spawn(proc, arg);
+  cmd.on('error', err => console.error(`${err.stack}`));
+  cmd.stdout.on('data', data => console.log(`${data}`));
+  cmd.stderr.on('data', data => console.error(`STDERR: ${data}`));
+  cmd.on('close', code =>
+    console.log(`Child process exited with the code: ${code}`)
+  );
+};
 
-process.stdin.resume();
-process.stdin.setEncoding('utf8');
-process.stdin.on('data', str => {
-  str = str.trim();
-  if (str === "q" || str === "quit") {
-		console.log("what")
-		process.exit()
-	}
-  let arr = str.split(' ');
-  console.log(arr)
-  const cmd = cp.spawn("echo", arr);
-  cmd.stdout.setEncoding('utf8');
-cmd.stdout.on('data', vals => console.log(vals));
-});
+let listenerStart = () => {
+  process.stdin.resume();
+  process.stdin.setEncoding('utf8');
+  processGenerator();
+};
 
+let processGenerator = () => {
+  process.stdin.on('data', data => {
+    data = data.trim();
+    if (data === 'q' || data === 'quit') {
+      console.log('Goodbye.');
+      listenerEnd();
+    }
+    const dataArr = data.split(' ');
+    const newProcess = dataArr[0];
+    const args = dataArr.slice(1);
+    newCP(newProcess, args);
+  });
+};
 
+let listenerEnd = () => {
+  process.exit();
+};
+
+listenerStart();
